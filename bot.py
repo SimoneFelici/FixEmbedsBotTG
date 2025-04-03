6e5e290a047a820ed2693dca39aada30c82f2450
Blue <felicisimone9@gmail.com>
2025-04-03 14:37:09 +0200

    Bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+import re
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
+
+TOKEN = "BOT_TOKEN"
+
+def convert_links(text):
+
+    # Instagram
+    text = re.sub(r'https?://(www\.)?instagram\.com', r'https://ddinstagram.com', text)
+
+    # Twitter
+    text = re.sub(r'https?://(www\.)?x\.com', r'https://fixupx.com', text)
+    text = re.sub(r'https?://(www\.)?twitter\.com', r'https://fxtwitter.com', text)
+
+    # TikTok
+    text = re.sub(r'https?://(www\.)?tiktok\.com', r'https://d.tnktok.com', text)
+
+    # YouTube (nocookie)
+    text = re.sub(r'https?://(www\.)?youtube\.com', r'https://www.youtube-nocookie.com', text)
+
+    return text
+
+async def link_converter(update, context):
+    original_text = update.message.text
+    converted_text = convert_links(original_text)
+    if original_text != converted_text:
+        await update.message.reply_text(converted_text)
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & (filters.Entity('url') | filters.Entity('text_link')), link_converter))
+app.run_polling()
